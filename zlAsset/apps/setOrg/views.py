b4b4e4d7671c97495bb82a77f9601eda7b307448
zili.li <dl1548@sina.com>
2019-04-19 00:57:38 +0800

    修改忽略

--- zlAsset/apps/setOrg/views.py
+++ zlAsset/apps/setOrg/views.py
@@ -233,3 +233,4 @@ def modify_member_action(request):
         return render(request,'setOrg/index.html',{'member_data':member_data })
     else:
         return HttpResponseRedirect ('/setData/index/')
+`
